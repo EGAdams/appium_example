2b82ec361149a13fdcc755167638a35282713c62
egadams <31349790+EGAdams@users.noreply.github.com>
2023-02-10 15:44:29 -0500

    baseparent with new user tests good.

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ class AutomationExpert {
 			hostname: '0.0.0.0',
 			capabilities: {
 				platformName: "Android",
-				platformVersion: "10",
+				platformVersion: "11",
 				"appium:deviceName": "R58MC1M2H9P",
-				"appium:app": "C:\\Users\\EG\\AndroidStudioProjects\\floridascarwash\\release\\app-release.apk",
-				"appium:appPackage": "com.awm.mcba.floridascarwash",
+				"appium:app": "C:\\Users\\EG\\AndroidStudioProjects\\baseparent\\app\\build\\outputs\\apk\\release\\app-release.apk",
+				"appium:appPackage": "com.awm.mcba.basecopy",
 				"appium:appActivity": "MainActivity",
 				"appium:automationName": "UiAutomator2",
 				"appium:chromedriverExecutable": "C:\\Android\\chromedriver.exe",
@@ -29,26 +29,25 @@ class AutomationExpert {
 		const chatButton = await client.$( '//*[@class="mcba_button fas fa-comments"]' );
 		await chatButton.click();
 
-		// switch to native context
-		await client.switchContext( contexts[ 0 ] );
+		await client.switchContext( contexts[ 0 ] );	// switch to native context
 
 		// wait for name field in Android widget, not webview
-		const first_name = await client.$( '//*[@resource-id="com.awm.mcba.floridascarwash:id/field_fname"]' );
-		await first_name.setValue( "Hans" );
-		const last_name = await client.$( '//*[@resource-id="com.awm.mcba.floridascarwash:id/field_lname"]' );
-		await last_name.setValue( "Schulz" );
-		const email = await client.$( '//*[@resource-id="com.awm.mcba.floridascarwash:id/field_email"]' );
-		await email.setValue( "hans@gmail.com" );
-		const password = await client.$( '//*[@resource-id="com.awm.mcba.floridascarwash:id/field_password"]' );
+		const first_name = await client.$( '//*[@resource-id="com.awm.mcba.basecopy:id/field_fname"]' );
+		await first_name.setValue( "Giz" );
+		const last_name = await client.$( '//*[@resource-id="com.awm.mcba.basecopy:id/field_lname"]' );
+		await last_name.setValue( "Elle" );
+		const email = await client.$( '//*[@resource-id="com.awm.mcba.basecopy:id/field_email"]' );
+		await email.setValue( "giz@gmail.com" );
+		const password = await client.$( '//*[@resource-id="com.awm.mcba.basecopy:id/field_password"]' );
 		await password.setValue( "princess" );
-		const confirm_password = await client.$( '//*[@resource-id="com.awm.mcba.floridascarwash:id/field_confirm_password"]' );
+		const confirm_password = await client.$( '//*[@resource-id="com.awm.mcba.basecopy:id/field_confirm_password"]' );
 		await confirm_password.setValue( "princess" );
 		// click button with id "email_create_account_button"
-		const create_account_button = await client.$( '//*[@resource-id="com.awm.mcba.floridascarwash:id/email_create_account_button"]' );
+		const create_account_button = await client.$( '//*[@resource-id="com.awm.mcba.basecopy:id/email_create_account_button"]' );
 		await create_account_button.click();
 		
-		await client.pause( 10000 );
-		await client.deleteSession();
+		await client.pause( 30000 );
+		//await client.deleteSession();
 	}
 }
 
